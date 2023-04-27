f1357a008619291754cb554cc1a1f683cb5d21a6
Juha Järvinen <juha.jarvinen@bitools.fi>
2023-04-27 14:31:12 +0300

    Update insight.js

--- insight.js
+++ insight.js
@@ -28,6 +28,33 @@ function Insight(div, data, options) {
   inputSearch.oninput = (function (obj) { return function () { obj.options.filters.master = this.value.toLowerCase(); obj.render(); } })(this)
   this.menu_container.append(inputSearch);
 
+  // Settings container
+  // let selectorRows = document.createElement("select");
+  // let selectorCols = document.createElement("select");
+  // selectorRows.setAttribute('multiple', true);
+  // selectorCols.setAttribute('multiple', true);
+  // for (let i = 0; i < this.headers.length; i++)
+  // {
+  //   let optionRows = document.createElement('option');
+  //   let optionCols = document.createElement('option');
+  //   optionRows.value = i;
+  //   optionCols.value = i;
+  //   optionRows.textContent = this.headers[i];
+  //   optionCols.textContent = this.headers[i];
+  //   // optionRows.setAttribute('selected', true);
+  //   selectorRows.appendChild(optionRows);
+  //   selectorCols.appendChild(optionCols);
+  // }
+
+  // selectorRows.onchange = (function (obj) { return function () { 
+  //   console.log(selectorRows.options);
+  //   // obj.options.filters.master = this.value.toLowerCase();
+  //   // obj.render(); 
+  // }})(this)
+
+  // this.settings_container.append(selectorRows);
+  // this.settings_container.append(selectorCols);
+
  // Container render
  document.getElementById(div).replaceChildren();
  document.getElementById(div).append(
@@ -97,8 +124,11 @@ function Insight(div, data, options) {
     else { rows[key][2]++; } // Count
    }
    rows = Object.values(rows);
+
+   // Sorting
    rows.sort(function(a,b) { return a[0] < b[0] ? 1 : -1 });
 
+   // Rendering
    for (let i = 0; i < rows.length; i++)
    {
     let tr = document.createElement("tr");
@@ -107,7 +137,6 @@ function Insight(div, data, options) {
       let key = document.createElement("th"); key.textContent = rows[i][1][i2];
       tr.append(key);
     }
-
     let value = document.createElement("th"); value.textContent = rows[i][2];
     tr.append(value);
     tbody.append(tr);
@@ -116,21 +145,197 @@ function Insight(div, data, options) {
 
    // Footer
    let tr = document.createElement("tr");
-   let total_grand_label = document.createElement("th"); total_grand_label.textContent = 'Totals';
+   let total_grand_label = document.createElement("th"); total_grand_label.textContent = 'Totals'; total_grand_label.colSpan = this.options.pivot.rows.length;
    let total_grand_value = document.createElement("th"); total_grand_value.textContent = total_grand;
    tr.append(total_grand_label, total_grand_value);
    tfoot.append(tr);
   }
   else if (this.options.pivot.rows.length == 0 && this.options.pivot.cols.length > 0) // Cols
   {
+    let cols = {};
+    for (let i = 0; i < this.rows.length; i++)
+    {
+    let keys = [];
+    for (let i2 = 0; i2 < this.options.pivot.cols.length; i2++) {
+      keys.push(this.rows[i][this.options.pivot.cols[i2]]);
+    }
+    key = keys.join('|');
+    if (typeof cols[key] == 'undefined') { cols[key] = [key, keys, 1]; }
+    else { cols[key][2]++; } // Count
+    }
+    cols = Object.values(cols);
+
+    // Sorting
+    cols.sort(function(a,b) { return a[0] > b[0] ? 1 : -1 });
+    
+    // Headers
+    for (let i = 0; i < this.options.pivot.cols.length; i++)
+    {
+      let tr = document.createElement("tr");
+      let name = document.createElement("th"); name.textContent = this.headers[this.options.pivot.cols[i]];
+      tr.append(name);
+      for (let i2 = 0; i2 < cols.length; i2++)
+      {
+        let label = document.createElement("th");
+        label.textContent = cols[i2][1][i];
+        tr.append(label);
+      }
+      if (i == 0) {
+        let label = document.createElement('th');
+        label.textContent = 'Totals';
+        label.rowSpan = this.options.pivot.cols.length;
+        tr.append(label);
+      }
+      thead.append(tr);
+    }
+
+    // Footer
+    let tr = document.createElement("tr");
+    let total_grand_label = document.createElement("th"); total_grand_label.textContent = 'Totals';
+    tr.append(total_grand_label);
+
+    for (let i = 0; i < cols.length; i++)
+    {
+      let value = document.createElement("th");
+      value.textContent = cols[i][2];
+      tr.append(value);
+    }
+
+    let total_grand_value = document.createElement("th"); total_grand_value.textContent = total_grand;
+    tr.append(total_grand_value);
+    tfoot.append(tr);
   }
   else // Rows & Cols
   {
-   let tr = document.createElement("tr");
-   let total_grand_label = document.createElement("th"); total_grand_label.textContent = 'Totals';
-   let total_grand_value = document.createElement("th"); total_grand_value.textContent = total_grand;
-   tr.append(total_grand_label, total_grand_value);
-   tfoot.append(tr);
+    // Data
+    let cols = {};
+    let rows = {};
+    let matrix = {};
+    for (let i = 0; i < this.rows.length; i++) {
+      let col_keys = [];
+      let row_keys = [];
+      let matrix_keys = [];
+      for (let i2 = 0; i2 < this.options.pivot.cols.length; i2++) { col_keys.push(this.rows[i][this.options.pivot.cols[i2]]); }
+      for (let i2 = 0; i2 < this.options.pivot.rows.length; i2++) { row_keys.push(this.rows[i][this.options.pivot.rows[i2]]); }
+      let col_key = col_keys.join('|');
+      let row_key = row_keys.join('|');
+      let matrix_key = row_key + '|' + col_key;
+      if (typeof cols[col_key] == 'undefined') { cols[col_key] = [col_key, col_keys, 1]; } else { cols[col_key][2]++; } // Count
+      if (typeof rows[row_key] == 'undefined') { rows[row_key] = [row_key, row_keys, 1]; } else { rows[row_key][2]++; } // Count
+      if (typeof matrix[matrix_key] == 'undefined') { matrix[matrix_key] = 1; } else { matrix[matrix_key]++; } // Count
+    }
+    cols = Object.values(cols);
+    rows = Object.values(rows);
+
+    // Sorting
+    let type = 0;
+    let order = true;
+    if (this.options.pivot.sort.cols.type == 'total') { type = 2; } else { type = 0; }
+    if (this.options.pivot.sort.cols.order) { cols.sort(function(a,b) { return a[type] > b[type] ? 1 : -1 }); }
+    else { cols.sort(function(a,b) { return a[type] < b[type] ? 1 : -1 }); }
+    
+    if (this.options.pivot.sort.rows.type == 'total') { type = 2; } else { type = 0; }
+    if (this.options.pivot.sort.rows.order) { rows.sort(function(a,b) { return a[type] > b[type] ? 1 : -1 }); }
+    else { rows.sort(function(a,b) { return a[type] < b[type] ? 1 : -1 }); }
+    // Matrix value sorting TODO
+    
+    // Headers
+    for (let i = 0; i < this.options.pivot.cols.length; i++) {
+      let tr = document.createElement("tr");
+      let label = document.createElement("th");
+      label.textContent = this.headers[this.options.pivot.cols[i]];
+      label.colSpan = this.options.pivot.rows.length;
+      // Label click sorting
+      label.onclick = (function (obj) { return function () {
+        if (obj.options.pivot.sort.cols.type == 'label') { obj.options.pivot.sort.cols.order = !obj.options.pivot.sort.cols.order; }
+        else { obj.options.pivot.sort.cols.type = 'label'; obj.options.pivot.sort.cols.order = true; }
+        obj.render();
+      }})(this);
+      tr.append(label);
+
+      for (let i2 = 0; i2 < cols.length; i2++) {
+        let label = document.createElement("th");
+        label.textContent = cols[i2][1][i];
+        if ((i+1) == this.options.pivot.cols.length) { label.rowSpan = 2; }
+        tr.append(label);
+      }
+      if (i == 0) {
+        let label = document.createElement('th');
+        label.textContent = 'Totals';
+        label.rowSpan = this.options.pivot.cols.length + 1;
+
+        // Totals click sorting
+        label.onclick = (function (obj) { return function () {
+          if (obj.options.pivot.sort.rows.type == 'total') { obj.options.pivot.sort.rows.order = !obj.options.pivot.sort.rows.order; }
+          else { obj.options.pivot.sort.rows.type = 'total'; obj.options.pivot.sort.rows.order = true; }
+          obj.render();
+        }})(this);
+        tr.append(label);
+      }
+      thead.append(tr);
+    }
+
+    // Row labels
+    let tr2 = document.createElement('tr');
+    for (let i = 0; i < this.options.pivot.rows.length; i++) {
+      let label = document.createElement('th');
+      label.textContent = this.headers[this.options.pivot.rows[i]];
+      // Label click sorting
+      label.onclick = (function (obj) { return function () {
+        if (obj.options.pivot.sort.rows.type == 'label') { obj.options.pivot.sort.rows.order = !obj.options.pivot.sort.rows.order; }
+        else { obj.options.pivot.sort.rows.type = 'label'; obj.options.pivot.sort.rows.order = true; }
+        obj.render();
+      }})(this);
+      tr2.append(label);
+    }
+    thead.append(tr2);
+
+    // Body
+    for (let i = 0; i < rows.length; i++) {
+      let tr = document.createElement("tr");
+      // Row label
+      for (let i2 = 0; i2 < rows[i][1].length; i2++) {
+        let label = document.createElement("th");
+        label.textContent = rows[i][1][i2];
+        tr.append(label);
+      }
+      // Matrix values
+      for (let i2 = 0; i2 < cols.length; i2++) {
+        let matrix_key = rows[i][0] + '|' + cols[i2][0];
+        let value = document.createElement("th");
+        value.textContent = matrix[matrix_key];
+        tr.append(value);
+      }
+      // Row total
+      let value = document.createElement("th");
+      value.textContent = rows[i][2];
+      tr.append(value);
+      tbody.append(tr);
+    }
+
+    // Footer
+    let tr = document.createElement("tr");
+    let label = document.createElement("th");
+    label.textContent = 'Totals';
+    label.colSpan = this.options.pivot.rows.length;
+    // Totals sorting
+    label.onclick = (function (obj) { return function () {
+      if (obj.options.pivot.sort.cols.type == 'total') { obj.options.pivot.sort.cols.order = !obj.options.pivot.sort.cols.order; }
+      else { obj.options.pivot.sort.cols.type = 'total'; obj.options.pivot.sort.cols.order = true; }
+      obj.render();
+    }})(this);
+    tr.append(label);
+
+    for (let i = 0; i < cols.length; i++) {
+      let value = document.createElement("th");
+      value.textContent = cols[i][2];
+      tr.append(value);
+    }
+
+    let value = document.createElement("th");
+    value.textContent = total_grand;
+    tr.append(value);
+    tfoot.append(tr);
   }
 
   table.append(thead);
